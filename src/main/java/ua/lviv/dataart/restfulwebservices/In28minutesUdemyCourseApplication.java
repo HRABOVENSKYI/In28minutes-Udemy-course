1db378f446ff0b53003ece3ef0fc8ddad139e275
Teodor Hrabovenskyi <teodor.hrabovenskyi@dataart.com>
2022-03-22 21:54:04 +0200

    Init proj

---
+++ src/main/java/ua/lviv/dataart/restfulwebservices/In28minutesUdemyCourseApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.dataart.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28minutesUdemyCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(In28minutesUdemyCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/lviv/dataart/restfulwebservices/In28minutesUdemyCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.dataart.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28minutesUdemyCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
