a8d464cf8dffa4a6f9887a24786bc125cefa9120
Teodor Hrabovenskyi <teodor.hrabovenskyi@dataart.com>
2022-03-24 11:37:48 +0200

    Versioning

---
+++ src/main/java/ua/lviv/dataart/restfulwebservices/versioning/Name.java
@@ -0,0 +1,15 @@
+package ua.lviv.dataart.restfulwebservices.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Name {
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/ua/lviv/dataart/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,14 @@
+package ua.lviv.dataart.restfulwebservices.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PersonV1 {
+    private String name;
+}
